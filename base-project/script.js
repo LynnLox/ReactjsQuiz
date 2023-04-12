1587b7a795faa67f3144f194b114656ec8f5d2ec
Lynn Locksley <107537250+LynnLox@users.noreply.github.com>
2023-04-12 09:07:37 +0530

    Add files via upload

---
+++ base-project/script.js
@@ -0,0 +1,66 @@
+const quizData = [
+    {
+        question: 'What is a react.js in MVC?',
+        a: 'Middleware',
+        b: 'Router',
+        c: 'Model',
+        d: 'Controller',
+        correct: 'd'
+    },
+    {
+        question: 'What is Redux in React JS?',
+        a: 'A front-end library for building user interfaces',
+        b: 'A state management tool for managing data in React',
+        c: 'A framework for building mobile apps',
+        d: 'A database management system',
+        correct: 'b'
+    },
+    {
+        question: 'What is JSX in React JS?',
+        a: 'A programming language',
+        b: 'A database management system',
+        c: 'A syntax extension for JavaScript',
+        d: 'A data structure in React',
+        correct: 'c'
+    },
+    {
+        question: 'Which directive can be used to write AngularJS expressions?',
+        a: 'ng-expression',
+        b: 'ng-bind',
+        c: 'ng-statement',
+        d: 'ng-bindexpression',
+        correct: 'b'
+    },
+    {
+        question: 'How can you access the state of a component from inside of a member function?',
+        a: 'this.values',
+        b: 'this.prototype.stateValue',
+        c: 'this.state',
+        d: 'this.getState()',
+        correct: 'a'
+    },
+    {
+        question: 'What does the "webpack" command do?',
+        a: 'Transpiles all the Javascript down into one file',
+        b: 'Runs react local development server',
+        c: 'A module bundler',
+        d: 'None of the above',
+        correct: 'c'
+    },
+    {
+        question: 'Who Develop React.js?',
+        a: 'Apple',
+        b: 'Google',
+        c: 'Twitter',
+        d: 'Facebook',
+        correct: 'd'
+    },
+    {
+        question: 'Which of the following is correct about prop in react?',
+        a: 'props',
+        b: 'render with arguments',
+        c: 'setState',
+        d: 'PropTypes',
+        correct: 'a'
+    }
+]
